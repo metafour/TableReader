6101777d16b4b8855b5f6afd36b58fd07134d815
Camron Schwoegler <camron@metafour.org>
2013-10-28 09:31:20 -0600

    Initial Commit

---
+++ TableReader/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  TableReader
+//
+//  Created by Camron Schwoegler on 10/28/13.
+//  Copyright (c) 2013 Camron Schwoegler. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
